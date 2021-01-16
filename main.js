03aa73e5f7365e93c0e4fae6f55721c0266fac03
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-01-16 11:58:33 -0600

    created some functions

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 console.log("Gabby");
+
+// function sayHello() {
+//   console.log('Hello!');
+// }
+
+// const sayHello = function() {
+//   console.log('Yo!');
+// }
+
+// const sayHello = () => {
+//   console.log('Hey!');
+// }
+// sayHello();
+
+// FUNCTIONS 
+
+// function logHello(name) {
+//   console.log(`Hello, ${name}`);
+// }
+
+// logHello('Gabby');
+
+const addsTwoNumbers = (num1, num2) => {
+  const sum = num1 + num2;
+  return sum;
+}
+
+// console.log(addsTwoNumbers(5,7));
+
+console.log(addsTwoNumbers(2, 1));
