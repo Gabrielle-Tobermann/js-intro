0b17683a2d6635aa270841c8822b978fd827e4b6
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-01-16 11:08:04 -0600

    added variables, data types, math into js file

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
 console.log("Gabby");
+
+let firstName = "Beyonce";
+let lastName = "Knowles";
+
+// console.log(firstName + lastName);
+// console.log(`${firstName} ${lastName}`);
+
+// VARIABLES
+
+firstName = 'Gabby';
+lastName = 'Tobermann'; 
+
+// console.log(firstName + lastName);
+// console.log(`${firstName} ${lastName}`);
+
+// const favoriteFood = 'Tacos';
+// console.log(favoriteFood);
+
+// favoriteFood = 'Noodles';
+// console.log(favoriteFood);
+
+
+// START DATA TYPES
+
+const dog = 'bark';
+// console.log(typeof dog);
+
+const answer = 33;
+const negative = -14;
+// console.log(typeof answer, typeof negative);
+
+const iLoveJavascript = true;
+// console.log(typeof iLoveJavascript);
+
+// MAAAAAATH
+console.log(Math.pow(2,2));
+console.log(Math.pow(3,2));
+console.log(Math.round(6.5)); //7
+console.log(Math.round(6.45)); //6
+console.log(Math.floor(6.99)); //6
+console.log(Math.ceil(6.001)); //7
